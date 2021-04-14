2930b35682ba33d20bb274d87cd3f188053fa4c6
Khays <37419938+Magras13@users.noreply.github.com>
2021-04-14 22:46:50 +0300

    Add files via upload

---
+++ WEndex_Taxi/cmake-build-debug/CMakeFiles/WEndex_Taxi.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WEndex_Taxi.dir/main.cpp.obj"
+  "WEndex_Taxi.exe"
+  "WEndex_Taxi.exe.manifest"
+  "WEndex_Taxi.pdb"
+  "libWEndex_Taxi.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WEndex_Taxi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
